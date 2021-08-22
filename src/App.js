932dbb28cf2ef652bf2796ad6b6698b59b0adcc3
sameera <abeysekaramadushanka14@gmail.com>
2021-08-22 21:32:11 +0530

    top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import DataFetching from './DataFetching';
+import ScrollToTop from "react-scroll-to-top";
+
 function App() {
   return (
     <div className="App">
       <b className="mainTitle">Movie plaza</b>
         <DataFetching/>
+        <ScrollToTop smooth color="#6f00ff" />
     </div>
   );
 }
